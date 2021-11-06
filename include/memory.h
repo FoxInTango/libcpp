0f72e7749e4663f7fd7853297f5c22547129656d
sparrow.wsl <foxintango@hotmail.com>
2021-11-06 23:44:31 +0800

    memory

--- include/memory.h
+++ include/memory.h
@@ -4,9 +4,17 @@
 
 EXTERN_C_BEGIN
 namespaceBegin(foxintango)
-void* memnew(const unsigned int& size);
-void  memdel(void* p);
-inline void  memcpy(void* s,void* t,const unsigned int& size);
+void* memnew(const unsigned int& size) {
+    return new char[size];
+}
+void  memdel(void* p) {
+    delete[] (char*)p;
+}
+
+inline void  memcpy(void* s,void* t,const unsigned int& size) {
+
+}
+
 inline void memclr(void* target,const unsigned int& length,const char& value = 0) {
     if(target) {
         for(unsigned int i = 0;i < length;i ++) {
