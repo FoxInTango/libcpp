8383f2168a0263a0dfa2d30ca120045205d17360
Jack.WSL <foxintango@yeah.net>
2022-12-15 23:29:02 +0800

    alpine

--- src/libcpp.cpp
+++ src/libcpp.cpp
@@ -21,5 +21,5 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-#include "../include/libcpp.h"
+#include "libcpp.h"
 using namespace foxintango;
