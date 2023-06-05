dc3f6a2b9c35e68e5b4f608fcb754d76a2d7e318
Jack.WSL <foxintango@yeah.net>
2023-06-05 21:21:19 +0800

    alpine

--- src/memory.h
+++ src/memory.h
@@ -7,7 +7,7 @@ namespaceBegin(foxintango)
 inline void* memnew(const Size& size) {
     return new char[size];
 }
-inline void  memdel(void* p) {
+inline void  memdel(const void* p) {
     delete[] (char*)p;
 }
 
@@ -16,7 +16,7 @@ inline void  memcpy(void* s,void* t,const unsigned int& size) {
 
 }
 */
-inline void memclr(Address target, Size& length,const char& value = 0) {
+inline void memclr(const Address target, Size& length,const char& value = 0) {
     if(target) {
         for(Index i = 0;i < length;i ++) {
            ((char*)target)[i] = value;
