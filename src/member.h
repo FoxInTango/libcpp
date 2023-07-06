eff4cdbbb8a4d2a1039b62059f4a2bdd03ece9eb
Jack.WSL <foxintango@yeah.net>
2023-07-06 17:42:24 +0800

    alpine

--- src/member.h
+++ src/member.h
@@ -9,12 +9,15 @@ public:
 public:
     Member(){}
     Member(const T& t) { this->t = t;}
+    Member(const T* t) { this->t = *t; }
     Member(const Member& m) { this->t = m.t; }
 public:
-    Member& operator =(const T& t){ this->t = t; return *this; }
+    Member& operator =(const T& t){ this->t =  t; return *this; }
+    Member& operator =(const T* t){ this->t = *t; return *this; }
     Member& operator =(const Member& m) { this->t = m.t; return *this; }
 public:
     operator T() { return this->t; }
+    operator T*() { return &this->t; }
 };
 namespaceEnd
 #endif
